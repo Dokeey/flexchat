49c665576067d81b70fc60a3708c069cbd0bf365
dokeey <ehdgnv@gmail.com>
2020-09-08 06:59:11 +0000

    Updates

---
+++ precache-manifest.1e5965d7ffa3aacba04e2980a5e053ed.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b82d12e0cc66a4434ede361ed477b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "757842d22562f2e5e5e8",
+    "url": "/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "6a343bc1c7485b74ffa6",
+    "url": "/static/css/main.79b65b25.chunk.css"
+  },
+  {
+    "revision": "757842d22562f2e5e5e8",
+    "url": "/static/js/2.8b76329c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8b76329c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a343bc1c7485b74ffa6",
+    "url": "/static/js/main.57ecb318.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "e82d095f1b618d80b40e86857fc3002d",
+    "url": "/static/media/chatback.e82d095f.png"
+  },
+  {
+    "revision": "ad21ba202563483d62cd09b89c7ea6a0",
+    "url": "/static/media/flexchatlogo.ad21ba20.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/flexchat/precache-manifest.1d2e01ec9b6c7673555ef1754773e9de.js"
+  "/precache-manifest.1e5965d7ffa3aacba04e2980a5e053ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flexchat/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
